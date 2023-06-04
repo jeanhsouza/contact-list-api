d20973514b401237e15d0630604c4544fcc47703
Jean Souza <jeanhsouza@gmail.com>
2023-06-04 15:54:48 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import {usersRoutes,loginRoute,  } from "./routers";
 import { contactsRoutes } from "./routers/contacts.router";
 
 const app: Application = express();
-app.use(express.json());
 app.use(cors())
+app.use(express.json());
 
 app.use("/users", usersRoutes);
 app.use("/login", loginRoute);
