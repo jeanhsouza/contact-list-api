2d82b4c625e46dbf61a411f30776165c86c3bf4a
Jean Souza <jeanhsouza@gmail.com>
2023-06-11 21:31:26 -0300

    fix:Docs4

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,21 @@ import express, { Application } from "express";
 import { handleErrors } from "./errors";
 import { usersRoutes, loginRoute } from "./routers";
 import { contactsRoutes } from "./routers/contacts.router";
-const path = require("path");
 
 const app: Application = express();
-const docsPath = path.join(__dirname, "./docs");
+const path = require("path");
+const docsPath = path.join(__dirname, "docs");
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRoutes);
 app.use("/login", loginRoute);
 app.use("/contacts", contactsRoutes);
-app.use("/docs", express.static(docsPath));
+app.use('/docs', express.static(docsPath));
+
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(docsPath, 'index.html'));
+});
 
 app.use(handleErrors);
 
